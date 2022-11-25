c27c896595a383a6011f5e1f4f34bdad36ff1903
lollor <loryraia2003@gmail.com>
2022-11-26 00:54:49 +0100

    Fixato il problema del titolo della pagina della squadra

--- app/squadre/[squadra]/head.tsx
+++ app/squadre/[squadra]/head.tsx
@@ -1,8 +1,29 @@
-export default async function Head(){
+import { Squadra } from "../../../typings";
+
+type ResponseSquadra = {
+   status: boolean,
+   message: string,
+   result: Squadra
+}
+
+type PageProps = {
+   params: {
+      squadra: string
+   }
+}
+
+const fetchSquadra = async (nomeLink: string) => {
+   const res = await fetch("https://" + process.env.OLD_URL + "/api/squadra/" + nomeLink, { cache: "force-cache" });
+   const { result: squadra }: ResponseSquadra = await res.json();
+   return squadra.nome;
+}
+
+export default async function Head({ params: { squadra: nomeLinkSquadra } }: PageProps){
+   const squadra = await fetchSquadra(nomeLinkSquadra);
    return (
       <>
-         <title>A.S.D. Valera - Squadra</title>
-         <meta name="description" content={`A.S.D. Valera - Squadra`} />
+         <title>{`A.S.D. Valera - ${squadra}`}</title>
+         <meta name="description" content={`A.S.D. Valera - ${squadra}`} />
       </>
    );
 }
