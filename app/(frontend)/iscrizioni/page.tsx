f893131c040fdeaec560026c29fdb35c128fc4f9
Lorenzo Raia <loryraia2003@gmail.com>
2023-05-31 22:10:58 +0200

    Messe le pre-iscrizioni. Cambiato in 23/24

--- app/(frontend)/iscrizioni/page.tsx
+++ app/(frontend)/iscrizioni/page.tsx
@@ -5,8 +5,17 @@ export default function Page(){
     return (
         <>
             <div className="sezione">
-                <h1 className="titoletto text-red-500 lg:text-5xl pb-3 italic">ISCRIZIONI STAGIONE SPORTIVA 2022/2023</h1>
+                <h1 className="titoletto text-red-500 lg:text-5xl pb-3 italic">ISCRIZIONI STAGIONE SPORTIVA 2023/2024</h1>
                 <div className="md:flex gap-8 justify-between my-[30px]">
+                    <div className="lg:w-2/3">
+                        <h1 className="text-center text-2xl font-bold md:text-3xl">PRE-ISCRIZIONI</h1>
+                        <div className="my-4 border border-opacity-50  mx-auto block shadow-2xl">
+                            <img src={logo.src} className="lg:h-[450px] h-[150px] object-contain mx-auto"></img>
+                            <div className="md:flex justify-around px-8">
+                                <a href={"/files/iscrizioni/preiscr_minorenne.pdf"} target="_blank" className="block text-blue-400 md:text-[20px] lg:text-[26px] md:inline-block text-center pb-2 font-bold">Minorenne</a>
+                            </div>
+                        </div>
+                    </div>
                     <div className="lg:w-2/3">
                         <h1 className="text-center text-2xl font-bold md:text-3xl">ATLETI</h1>
                         <div className="my-4 border border-opacity-50  mx-auto block shadow-2xl">
@@ -17,16 +26,7 @@ export default function Page(){
                             </div>
                         </div>
                     </div>
-                    <div className="lg:w-2/3">
-                        <h1 className="text-center text-2xl font-bold md:text-3xl">DIRIGENTI</h1>
-                        <div className="my-4 border border-opacity-50 mx-auto block shadow-2xl">
-                            <img src={logo.src} className="lg:h-[450px] h-[150px] object-contain mx-auto"></img>
-                            <div className="md:flex justify-around px-8">
-                                <a href={"/files/iscrizioni/di_minorenne.pdf"} target="_blank" className="block text-blue-400 md:text-[20px] lg:text-[26px] md:inline-block text-center pb-2 font-bold">Minorenne {"(≥16 anni)"}</a>
-                                <a href={"/files/iscrizioni/di_maggiorenne.pdf"} target="_blank" className="block text-blue-400 md:text-[20px] lg:text-[26px] md:inline-block text-center pb-2 font-bold">Maggiorenne</a>
-                            </div>
-                        </div>
-                    </div>
+                    
                 </div>
             </div>
             <div className="sezione">
@@ -35,6 +35,16 @@ export default function Page(){
                         <h1 className="text-center text-2xl font-bold md:text-3xl">QUOTE D'ISCRIZIONE</h1>
                         <img src={quote.src} className="h-[350px] w-auto mx-auto lg:h-[450px] my-4 border border-opacity-50 shadow-2xl" ></img>
                     </div> */}
+                    <div className="lg:w-1/3">
+                        <h1 className="text-center text-2xl font-bold md:text-3xl">DIRIGENTI</h1>
+                        <div className="my-4 border border-opacity-50 mx-auto block shadow-2xl">
+                            <img src={logo.src} className="lg:h-[350px] h-[150px] object-contain mx-auto"></img>
+                            <div className="md:flex justify-around px-8">
+                                <a href={"/files/iscrizioni/di_minorenne.pdf"} target="_blank" className="block text-blue-400 md:text-[20px] lg:text-[22px] md:inline-block text-center pb-2 font-bold">Minorenne {"(≥16 anni)"}</a>
+                                <a href={"/files/iscrizioni/di_maggiorenne.pdf"} target="_blank" className="block text-blue-400 md:text-[20px] lg:text-[22px] md:inline-block text-center pb-2 font-bold">Maggiorenne</a>
+                            </div>
+                        </div>
+                    </div>
                     <div>
                         <h1 className="text-center text-2xl font-bold md:text-3xl">VADEMECUM</h1>
                         <div className="my-4 border border-opacity-50 mx-auto block shadow-2xl md:flex flex-col justify-center md:h-[450px]">
